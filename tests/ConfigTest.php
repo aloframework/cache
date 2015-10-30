948e9e8e1208e6b35fca1fa9b4e6844f16ec23a3
Alorel <a.molcanovas@gmail.com>
2015-10-30 11:02:22 +0000

    coverage PH

---
+++ tests/ConfigTest.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace AloFramework\Cache\Tests;
+
+    use AloFramework\Cache\Config\RedisConfig;
+    use PHPUnit_Framework_TestCase;
+
+    class ConfigTest extends PHPUnit_Framework_TestCase {
+
+        function testConfig() {
+            $this->assertEquals([RedisConfig::CFG_IP      => '127.0.0.1',
+                                 RedisConfig::CFG_PORT    => 6379,
+                                 RedisConfig::CFG_TIMEOUT => 300],
+                                (new RedisConfig())->getAll());
+        }
+    }
