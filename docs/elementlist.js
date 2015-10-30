7a85c9a801f58ff5b79600b18c94f318d3af68db
Alorel <a.molcanovas@gmail.com>
2015-10-30 10:59:37 +0000

    docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","AloFramework\\Cache\\CacheException"],["c","AloFramework\\Cache\\CacheItem"],["c","AloFramework\\Cache\\ClientInterface"],["c","AloFramework\\Cache\\Clients\\RedisClient"],["c","AloFramework\\Cache\\Config\\RedisConfig"],["c","ArrayAccess"],["c","Countable"],["c","DateTime"],["c","DateTimeInterface"],["c","Exception"],["c","IteratorAggregate"],["c","Redis"],["c","Traversable"]];
